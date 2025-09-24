474a2314d485df992c9c432ece23dc76924256ef
loraai3821 <loraai3821@gmail.com>
2025-09-24 11:44:00 +0500

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -45,7 +45,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "\n🌐Global prefix is: %1\n🛸Your group chat prefix is: %2"
+			myPrefix: "🌐Global prefix is: %1\n🛸Your group chat prefix is: %2"
 		}
 	},
 
